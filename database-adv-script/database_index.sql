11dc248a5ff1147fc7e65f11f74f5c2a60b7cf25
alex-waiganjo <alexwaiganjo934@gmail.com>
2024-12-02 23:08:32 +0300

    Index Performance

---
+++ database-adv-script/database_index.sql
@@ -0,0 +1,12 @@
+-- User Table
+CREATE INDEX index_user_email ON User(email); -- for quick lookup by email
+CREATE INDEX index_user_id ON User(user_id); -- for JOINs
+
+-- Booking Table
+CREATE INDEX index_booking_property_id ON Booking(property_id); -- for JOINs with Property
+CREATE INDEX index_booking_user_id ON Booking(user_id); -- for JOINs with User
+CREATE INDEX index_booking_status ON Booking(status); -- for filtering by booking status
+
+-- Property Table
+CREATE INDEX index_property_location ON Property(location); -- for filtering by location
+CREATE INDEX index_property_host_id ON Property(host_id); -- for JOINs with User
--- database-adv-script/index_performance.md
+++ database-adv-script/index_performance.md
@@ -7,3 +7,23 @@
 - Write SQL CREATE INDEX commands to create appropriate indexes for those columns and save them on database_index.sql
 
 - Measure the query performance before and after adding indexes using EXPLAIN or ANALYZE.
+
+
+# 1. High Usage Columns
+ - User Table:
+   - email
+   - user_id
+
+- Property Table:
+   - location
+   - host_id
+
+- Booking Table:
+   - property_id and user_id
+   - status
+
+# 2. Index Creation
+- Refer to database_index.sql
+
+# 3. Measure the query performance before and after adding indexes using EXPLAIN or ANALYZE
+    - EXPLAIN ANALYZE SELECT * FROM Booking WHERE user_id = 5;
