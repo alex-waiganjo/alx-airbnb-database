301429a71e2817ede43d00bdaaea7fae27088562
alex-waiganjo <alexwaiganjo934@gmail.com>
2024-12-02 23:09:51 +0300

    measure performance

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -10,3 +10,7 @@ CREATE INDEX index_booking_status ON Booking(status); -- for filtering by bookin
 -- Property Table
 CREATE INDEX index_property_location ON Property(location); -- for filtering by location
 CREATE INDEX index_property_host_id ON Property(host_id); -- for JOINs with User
+
+
+-- Measure performance
+EXPLAIN ANALYZE SELECT * FROM Booking WHERE user_id = 5;
